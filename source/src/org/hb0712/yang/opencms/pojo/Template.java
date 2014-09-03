c126e35e1055558c8f748a0e5690fea57fbd9b37
xieyang <xieyang@yeah.net>
2014-09-03 10:35:39 +0800

    Template的方法注释下

--- source/src/org/hb0712/yang/opencms/pojo/Template.java
+++ source/src/org/hb0712/yang/opencms/pojo/Template.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Template {
@@ -53,6 +54,7 @@ public class Template {
 		this.content = content;
 	}
 
+	@Transient
 	public String getFileName(){
 		return modelid + ".vm";
 	}
